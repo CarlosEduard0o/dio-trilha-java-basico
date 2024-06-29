f8f725c9a595e0b1810241a462036135bbe998aa
Carlos <carlos_eduardo_a.r@outlook.com>
2024-06-29 11:46:47 -0300

    feat: criacao do workspace java-terminal para realizar testes de execucao de codigo pelo terminal do windows e tambem a passagem de argumentospelo terminal ou launch.json

---
+++ TiposVariaveis/src/main/java/edu/carlos/variaveis/calculadora/Comentarios.java
@@ -0,0 +1,17 @@
+package edu.carlos.variaveis.calculadora;
+
+public class Comentarios {
+    public static void main(String[] args) {
+        //Comentário de uma linha
+        /*
+        Comentário multilinhas
+         */
+    }
+
+    /**
+     * Comentario para documentacao
+     */
+    public void metodo(){
+
+    }
+}
